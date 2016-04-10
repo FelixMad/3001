db1d4c76bd31789e2e37e10d9d5f6e08a3991fd6
felixmad <felix.garrido@gmail.com>
2016-04-10 20:59:34 +0200

    Modificación de puerto

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ gulp.task('server', function() {
   gulp.src('./build')
     .pipe(webserver({
       host: '0.0.0.0',
-      port: 8080,
+      port: 3001,
       fallback: 'index.html',
       livereload: true
     }))
